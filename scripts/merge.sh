5b36e895392fc2c279c4ad9ac1d9e73855792e6c
Ivan Pogrebnyak <ivan.pogrebnyak@gmail.com>
2017-06-09 18:52:00 +0200

    merge script

---
+++ scripts/merge.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+merger=~/work/bh_analysis/bin/merge_parts
+
+if [ "$1" == "--all" ]; then
+  for raw in *_raw.root; do
+    cooked=$(sed 's/_raw//' <<< $raw)
+    if [ ! -s "$cooked" ]; then
+      $merger $cooked $raw
+    fi
+  done
+fi
+
+for name in $(ls | sed -n 's/\(.*\)_\(B\|RS\|I\|V\)_raw\.root$/\1/p' \
+                 | sort | uniq)
+do
+  if [ ! -s "${name}_NLO.root" ]; then
+    $merger ${name}_NLO.root ${name}_*_raw.root
+  fi
+done
